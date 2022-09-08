7d425c2818094d76ec7434c9427240dc72236293
maeil99 <ismailmohdnorit@gmail.com>
2022-09-08 18:55:56 +0800

    added smart contract

---
+++ context/constant.ts
@@ -0,0 +1,8 @@
+import marketAbi from './GoodsMarketplace.json';
+
+// get from deployed smart contract
+export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+export const MarketAddressABI = marketAbi.abi;
+
+// 0x5FbDB2315678afecb367f032d93F642f64180aa3
