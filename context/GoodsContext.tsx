7cb78f85d534e587435953cdfbb1c360bb7e6fba
maeil99 <ismailmohdnorit@gmail.com>
2022-09-17 16:06:14 +0800

    fix upload image

--- context/GoodsContext.tsx
+++ context/GoodsContext.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import web3Modal from 'web3modal';
 import { ethers } from 'ethers';
 import axios from 'axios';
+// eslint-disable-next-line import/no-unresolved
+import { create as ipfsHTTPClient } from 'ipfs-http-client';
 
 import { MarketAddress, MarketAddressABI } from './constant';
 
@@ -13,12 +15,34 @@ interface ICreateContextProps {
   goodsCurrency: string;
   currentAccount: string;
   connectWallet?: () => Promise<void>;
+  // eslint-disable-next-line no-unused-vars
+  uploadToIPFS?: (file: File) => Promise<string | undefined>;
 }
+const projectId = process.env.NODE_ENV === 'development'
+  ? process.env.NEXT_PUBLIC_PROJECT_ID
+  : '2Et1N03iLLod4TEyJJ9Lf8dycqW';
+const projectSecret = process.env.NODE_ENV === 'development'
+  ? process.env.NEXT_PUBLIC_KEY_SECRET
+  : 'b98447f3a0be37903b9175632f56ed17';
+const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString(
+  'base64',
+)}`;
+const options = {
+  host: 'ipfs.infura.io',
+  protocol: 'https',
+  port: 5001,
+  headers: { authorization: auth },
+};
+const client = ipfsHTTPClient(options);
+const dedicatedEndPoint = process.env.NODE_ENV === 'development'
+  ? process.env.NEXT_PUBLIC_DEDICATED_GATEWAY_DEV
+  : 'https://rgsm-dev.infura-ipfs.io';
 
 export const GoodsContext = React.createContext<ICreateContextProps>({
   goodsCurrency: '',
   currentAccount: '',
   connectWallet: undefined,
+  uploadToIPFS: undefined,
 });
 
 export const GoodsProvider = ({ children }: IContextProps) => {
@@ -49,9 +73,22 @@ export const GoodsProvider = ({ children }: IContextProps) => {
     setCurrentAccount(accounts[0]);
     window.location.reload();
   };
+  // upload picture to IPFS
+  const uploadToIPFS = async (file: File) => {
+    try {
+      const added = await client.add({ content: file });
+      const url = `${dedicatedEndPoint}/ipfs/${added.path}`;
+      return url;
+    } catch (error) {
+      console.log(error);
+      console.log('Error uploading file to IPFS.');
+    }
+  };
 
   return (
-    <GoodsContext.Provider value={{ goodsCurrency, connectWallet, currentAccount }}>
+    <GoodsContext.Provider
+      value={{ goodsCurrency, connectWallet, currentAccount, uploadToIPFS }}
+    >
       {children}
     </GoodsContext.Provider>
   );
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const dedicatedEndPoint = process.env.NODE_ENV === 'development'
+  ? process.env.NEXT_PUBLIC_DEDICATED_GATEWAY_DEV
+  : 'https://rgsm-dev.infura-ipfs.io';
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    // set up domain for images
+    domains: [dedicatedEndPoint, 'rgsm-dev.infura-ipfs.io'],
+  },
 };
 
 module.exports = nextConfig;
