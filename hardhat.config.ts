2a48a9f06621ebe97df4905f6313c3bcc44a2cb9
maeil99 <ismailmohdnorit@gmail.com>
2022-11-01 14:04:47 +0800

    deploy smart contact to testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,10 +7,15 @@ import { readFileSync } from 'fs';
 const privateKey = readFileSync('.secret').toString().trim();
 
 const config: HardhatUserConfig = {
+  defaultNetwork: 'goerli',
   networks: {
     hardhat: {
       // for testing purpose
       chainId: 1337,
+    },
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/5UN4579oCmhZgvlQVhd-JHcH_CcQKn4Y',
+      accounts: [`0x${privateKey}`],
     },
   },
   solidity: '0.8.9',
