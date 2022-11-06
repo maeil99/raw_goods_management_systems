b733402439eb0e292b6fa9d136cd6fb317ae7e1e
maeil <83578234+maeil99@users.noreply.github.com>
2022-11-06 23:49:47 +0800

    Develop (#13)
    * fix minor bugs
    
    * deploy smart contact to testnet
    
    * fix minor bugs
    
    * Report a seller page (#10)
    
    * report-a-seller page as placeholder
    
    * install firebase
    
    * added sent data for report-a-seller page
    
    * fix some syntax
    
    * added listed page
    
    * comment all console log and fix minor some spelling
    
    * remove index.ts from function file
    
    * Report a seller page (#12)
    
    * report-a-seller page as placeholder
    
    * install firebase
    
    * added sent data for report-a-seller page
    
    * fix some syntax
    
    * added listed page
    
    * comment all console log and fix minor some spelling
    
    * display reported case
    
    * fix merge conflicty

--- components/views/goods-details/GoodsTab.tsx
+++ components/views/goods-details/GoodsTab.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 // eslint-disable-next-line import/no-unresolved
+import { useCollection } from '../../../shared/firebaseHooks/useCollection';
+// eslint-disable-next-line import/no-unresolved
 import removeUnderscore from '../../../shared/utils/removeUnderscore';
 // eslint-disable-next-line import/no-unresolved
 import { IContactDetailsProps } from '../../../types/contact.interface';
@@ -32,6 +34,9 @@ interface IGoodsTabProps {
 const GoodsTab = (productInfo: IGoodsTabProps) => {
   const { generalInfo, contactInfo, goodsDetails, seller, tokenURI, tokenId } = productInfo;
   const [currentTab, setCurrentTab] = useState<string>('general');
+  const listOfReport = useCollection({ databaseCollection: 'report' });
+  const reportedProduct = listOfReport && listOfReport.documents?.find((prod) => prod.seller === seller && prod.tokenId === tokenId);
+  // console.log('report prod: ', reportedProduct);
   // console.log('general info: ', generalInfo);
   // console.log('contact info: ', contactInfo);
   // console.log('goods details: ', goodsDetails);
@@ -45,6 +50,9 @@ const GoodsTab = (productInfo: IGoodsTabProps) => {
         break;
       case 'product':
         setCurrentTab('product');
+        break;
+      case 'report':
+        setCurrentTab('report');
         break;
       default:
         setCurrentTab('general');
@@ -74,6 +82,13 @@ const GoodsTab = (productInfo: IGoodsTabProps) => {
             btnType="button"
             handleClick={() => generateTab('product')}
           />
+          {reportedProduct && (
+            <BareButton
+              btnName="Reported Case"
+              btnType="button"
+              handleClick={() => generateTab('report')}
+            />
+          )}
         </div>
 
         <Button btnName="Report This Product" btnType="button" handleClick={() => router.push(`/report-a-seller?tokenURI=${tokenURI}&seller=${seller}&tokenId=${tokenId}`)} classStyles="rounded-xl mx-2 bg-red-500 hover:shadow-lg" />
@@ -328,6 +343,23 @@ const GoodsTab = (productInfo: IGoodsTabProps) => {
               </p>
             </div>
           </div>
+      )}
+      {currentTab === 'report' && reportedProduct && (
+      <div className="px-2 mt-3 flex flex-row space-x-2 text-sm minlg:text-lg">
+        <div className="flex flex-col space-y-2 pb-2 font-poppins dark:text-white text-nft-black-1 font-semibold">
+          <p>Number of reported case:</p>
+          <p>Comments:</p>
+        </div>
+
+        <div className="flex flex-col space-y-2 pb-2 font-poppins dark:text-white text-nft-black-1 font-normal">
+          <p>{reportedProduct.numberOfReport || 'NA'}</p>
+          <ol>
+            {reportedProduct.comment?.map((report, index) => (
+              <li>{`${index + 1}) ${report}`}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
       )}
     </>
   );
