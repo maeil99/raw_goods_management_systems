362a7cb4d43e56b6a0828d87e21e9d4faf06bf6f
maeil99 <ismailmohdnorit@gmail.com>
2022-11-17 00:19:48 +0800

    added view goods button

--- components/ReportCard.tsx
+++ components/ReportCard.tsx
@@ -1,28 +1,71 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable react/require-default-props */
-import React, { useState } from 'react';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 import MiniChevronDownIcon from '../assets/icons/MiniChevronDownIcon';
+import { IFormattedGoods } from '../types/goods.interface';
 
+interface IGoodsDetails extends IFormattedGoods {
+  i?: number;
+}
 type IReportCardProps = {
   seller: string;
   reportedCases: number;
   className?: string;
   children?: React.ReactNode;
+  filteredGoods?: IGoodsDetails;
 };
 
 const ReportCard = ({
   children,
   className,
-  seller,
+  seller: originSeller,
   reportedCases,
+  filteredGoods,
 }: IReportCardProps) => {
   const [toggleCard, setToggleCard] = useState(false);
+  const [prodDetails, setProdDetails] = useState<IGoodsDetails>({
+    price: '',
+    tokenId: 0,
+    seller: '',
+    owner: '',
+    tokenURI: null,
+    product: {
+      name: '',
+      category: 'chicken',
+      price: 0,
+      description: '',
+      weight: 0,
+      deliveryMethod: '',
+      deliveryPeriod: 0,
+      imageURI: '',
+      createdAt: '',
+    },
+    contact: {
+      contactName: '',
+      contactAddress: '',
+      contactEmail: '',
+      contactMOC: '',
+      contactPhoneNo: '',
+    },
+    goodsDetails: {},
+  });
+
+  useEffect(() => {
+    if (filteredGoods) {
+      setProdDetails(filteredGoods);
+    }
+  }, []);
+  //   console.log('filtered Goods: ', filteredGoods);
+
+  const { owner, product, seller, tokenId, tokenURI, contact, goodsDetails } = prodDetails;
+  const { chicken, meat, seafood, vegetable, fruit } = goodsDetails;
   return (
     <div
       className={`${className} bg-white dark:bg-nft-black-1 border border-gray-400 rounded-xl divide-y divide-solid sm:px-3 px-6 shadow-md`}
     >
       <div className="flex justify-between sm:mb-2 mb-4 sm:pt-3 pt-6 sm:text-sm text-lg sm:font-semibold font-bold">
-        <p>{seller}</p>
+        <p>{originSeller}</p>
         <div className="flex space-x-2">
           <div className="bg-red-500 py-2 rounded-lg px-2 text-white  sm:shadow-sm shadow-lg">
             <p>
@@ -43,6 +86,32 @@ const ReportCard = ({
       </div>
       <div className={`${toggleCard ? '' : 'hidden '}`}>
         <div className="flex flex-col divide-y divide-solid">{children}</div>
+        {prodDetails && prodDetails.seller === originSeller && (
+          <div className="flex justify-end sm:text-sm text-lg sm:font-medium font-semibold dark:text-nft-black-1 text-white px-2 pb-4">
+            <Link
+              href={{
+                pathname: '/goods-details',
+                query: {
+                  tokenId,
+                  tokenURI,
+                  owner,
+                  seller,
+                  ...product,
+                  ...contact,
+                  ...chicken,
+                  ...meat,
+                  ...seafood,
+                  ...vegetable,
+                  ...fruit,
+                },
+              }}
+            >
+              <div className="border p-4 rounded-lg bg-blue-400 shadow-md hover:shadow-xl hover:cursor-pointer">
+                View Product
+              </div>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
--- components/views/report-page/index.tsx
+++ components/views/report-page/index.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable import/no-unresolved */
 import { NextPage } from 'next';
 
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useCollection } from '../../../shared/firebaseHooks/useCollection';
 
 import ReportCard from '../../ReportCard';
 import { IReportUserProps } from '../../../types/firebase.interface';
 import Loader from '../../Loader';
 import ReportSearchBar from './ReportSearchBar';
+import { GoodsContext } from '../../../context/GoodsContext';
+import { IFormattedGoods } from '../../../types/goods.interface';
 
 const ReportPage: NextPage = () => {
   const [isLoadingGoods, setIsLoadingGoods] = useState<boolean>(true);
@@ -24,16 +26,22 @@ const ReportPage: NextPage = () => {
   const reportData = useCollection({ databaseCollection: 'report' });
   const sortedData = reportData.documents?.sort(compareFn) || [];
 
+  const { fetchGoods } = useContext(GoodsContext);
+  const [listOfGoods, setListOfGoods] = useState<IFormattedGoods[]>([]);
+
   useEffect(() => {
     const timeOut = setTimeout(() => {
-      if (!sortedData) return;
-      setListOfSeller(sortedData);
-      setListOfSellerCopy(sortedData);
-      setIsLoadingGoods(false);
+      if (!sortedData || !fetchGoods) return;
+      fetchGoods().then((items) => {
+        setListOfGoods(items);
+        setListOfSeller(sortedData);
+        setListOfSellerCopy(sortedData);
+        setIsLoadingGoods(false);
+      });
     }, 3000);
     return () => clearTimeout(timeOut);
   }, [sortedData]);
-
+  //   console.log('list of goods: ', listOfGoods);
   if (isLoadingGoods) {
     return (
       <div className="flex-start min-h-screen">
@@ -72,41 +80,50 @@ const ReportPage: NextPage = () => {
           clearSearch={onClearSearch}
         />
         <div className="py-2">
-          {listOfSeller.map((report) => (
-            <div className="py-2" key={report.id}>
-              <ReportCard
-                seller={report.seller || 'NA'}
-                reportedCases={report.numberOfReport || 0}
-              >
-                <div className="flex flex-col pb-4">
-                  <p>{`Product ID: ${report.tokenId || 'NA'}`}</p>
-                  <div className="flex justify-center">
-                    {report.imageURI && (
-                      <img
-                        src={report.imageURI}
-                        className="rounded mt-4"
-                        width={350}
-                      />
-                    )}
-                  </div>
-                  <div className="flex flex-col space-y-3 p-5   rounded-lg divide-y sm:text-sm text-lg">
-                    <p className="sm:font-medium font-semibold">Comments:</p>
-                    <div className="flex flex-col space-y-2 px-11 py-5">
-                      {report.comment
-                        && report.comment.map((comment, i) => (
-                          <div
-                            key={i}
-                            className="p-5 border shadow-lg rounded-lg"
-                          >
-                            <p>{`${i + 1}) ${comment}`}</p>
-                          </div>
-                        ))}
+          {listOfSeller.map((report) => {
+            if (!report.tokenId) return;
+            const filteredReportedGoods = listOfGoods.find(
+              (product) => product.tokenId === Number(report.tokenId),
+            );
+            // console.log('filtered prod: ', filteredReportedGoods);
+            return (
+              <div className="py-2" key={report.id}>
+                <ReportCard
+                  seller={report.seller || 'NA'}
+                  reportedCases={report.numberOfReport || 0}
+                  filteredGoods={filteredReportedGoods}
+                >
+                  <div className="flex flex-col pb-4">
+                    <p className="sm:font-medium font-semibold sm:text-sm text-lg">{`Product ID: ${report.tokenId || 'NA'}`}</p>
+                    <div className="flex justify-center">
+                      {report.imageURI && (
+                        <img
+                          src={report.imageURI}
+                          className="rounded mt-4"
+                          width={350}
+                        />
+                      )}
                     </div>
+                    <div className="flex flex-col space-y-3 p-5   rounded-lg divide-y sm:text-sm text-lg">
+                      <p className="sm:font-medium font-semibold">Comments:</p>
+                      <div className="flex flex-col space-y-2 px-11 py-5">
+                        {report.comment
+                          && report.comment.map((comment, i) => (
+                            <div
+                              key={i}
+                              className="p-5 border shadow-lg rounded-lg"
+                            >
+                              <p>{`${i + 1}) ${comment}`}</p>
+                            </div>
+                          ))}
+                      </div>
+                    </div>
+
                   </div>
-                </div>
-              </ReportCard>
-            </div>
-          ))}
+                </ReportCard>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
